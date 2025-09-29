7af1c7423e84f6b57804af07c66a7b33f2c6b526
Gogovski20 <vladimir_gogovski@hotmail.com>
2025-09-29 15:28:25 +0200

    Added entities

---
+++ DoctoralManagement.Domain/Entities/DoctoralProject.cs
@@ -0,0 +1,18 @@
+﻿namespace DoctoralManagement.Domain.Entities
+{
+    public class DoctoralProject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string ResearchArea { get; set; } = string.Empty;
+        public int EctsCredits { get; set; }
+
+        public int StudentId { get; set; }
+        // Navigation
+        public Student? Student { get; set; } 
+
+        public int MentorId { get; set; }
+        // Navigation
+        public Mentor? Mentor { get; set; }
+    }
+}
---
+++ DoctoralManagement.Domain/Entities/Mentor.cs
@@ -0,0 +1,13 @@
+﻿namespace DoctoralManagement.Domain.Entities
+{
+    public class Mentor
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        // Navigation
+        public ICollection<DoctoralProject> DoctoralProjects { get; set; } = new List<DoctoralProject>();
+    }
+}
